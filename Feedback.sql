e80ed4f7b461d824f0802c7fa88596f6e9102503
compscibaddie <601335@student.belgiumcampus.ac.za>
2025-07-16 10:08:45 +0200

    ADD appointment.sql, counselors.sql, feedback.sql

---
+++ Feedback.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  kekan
+ * Created: 16 Jul 2025
+ */
+
